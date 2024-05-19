6dcc567647cd479ffbc2b0e138cd12c91a8da1d2
serebrus143 <serebrus143@gmail.com>
2024-05-19 19:26:23 +0300

    ДЗ+

---
+++ src/service/EmployeeServiceImpl.java
@@ -0,0 +1,53 @@
+package service;
+
+import org.springframework.stereotype.Service;
+import model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final List<Employee> employeeList;
+
+    public EmployeeServiceImpl() {
+        this.employeeList = new ArrayList<>();
+    }
+
+    @.Override
+    public Employee add(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+        employeeList.add(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee remove(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
+            employeeList.remove(employee);
+            return employee;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Employee find(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+
+        if (employeeList.contains(employee)) {
+        return employee;
+        }
+
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Collection<Employee> findAll() {
+        return Collection.unmodifiableList(employeeList);
+    }
+}
