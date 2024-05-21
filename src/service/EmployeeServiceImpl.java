8339f801cbdc8c56483d1547f1b8f7c88a13e007
serebrus143 <serebrus143@gmail.com>
2024-05-21 21:38:39 +0300

    ДЗ+

--- src/service/EmployeeServiceImpl.java
+++ src/service/EmployeeServiceImpl.java
@@ -4,46 +4,53 @@ import org.springframework.stereotype.Service;
 import model.Employee;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final List<Employee> employeeList;
+    private final Map<String, Employee> employees = new HashMap<>();
 
     public EmployeeServiceImpl() {
-        this.employeeList = new ArrayList<>();
+        this.employees = new ArrayList<>();
     }
 
-    @.Override
+    @Override
     public Employee add(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
+        String key = buildKey(firstName, lastName);
+        if (employeeList.containsKey(key)) {
             throw new EmployeeAlreadyAddedException();
         }
-        employeeList.add(employee);
+        Employee employee = new Employee(firstName, lastName);
+        employees.put(key, employee);
         return employee;
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
-            employeeList.remove(employee);
-            return employee;
+        String key = buildKey(firstName, lastName);
+        if (employeeList.contains(key)) {
+            employees.remove(key);
+            return employees.remove(key);
         }
         throw new EmployeeNotFoundException();
     }
 
     @Override
     public Employee find(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-
-        if (employeeList.contains(employee)) {
-        return employee;
+        String key = buildKey(firstName, lastName);
+        if (employeeList.contains(key)) {
+        return employees.get(key);
         }
 
         throw new EmployeeNotFoundException();
+    }
+
+    private String buildKey(String name, String surname) {
+        return name + surname;
     }
 
     @Override
